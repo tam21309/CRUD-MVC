b236fabfe203da0b0b2c34ff3748d361650421f2
tam21309 <belltam240297@gmail.com>
2022-10-14 17:00:42 +0700

    MVC

---
+++ controllers/OrderController.php
@@ -0,0 +1,38 @@
+<?php
+    class OrderController {
+        
+        // Hien thi tat ca
+        public function index(){
+            
+        }
+
+        // Hien thi chi tiet
+        public function show(){
+            
+        }
+
+        // Hien thi form them moi
+        public function add(){
+            
+        }
+
+        //Xu ly form them moi
+        public function store(){
+            
+        }
+
+        // Hien thi form edit
+        public function edit(){
+            
+        }
+        //Xu ly edit
+        public function update(){
+            
+        }
+
+        // Phuong thuc delete
+        public function delete(){
+            
+        }
+        
+    }
